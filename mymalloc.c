89a17144dcb5a38f9fd78cc607eed30057344e98
ccarnesi <cfc80@ice.cs.rutgers.edu>
2019-10-04 11:40:11 -0400

    structure

--- mymalloc.c
+++ mymalloc.c
@@ -20,8 +20,28 @@ void* mymalloc(size_t size, char* file, int line){
         metadata* meta = (metadata*)myblock;
         if(meta->c != '#'){
                 //first time create sstruct managing block size - metadata size in use 0
+                meta->isUsed = 1;
+                meta->isManaging = size;
+                meta->prev = NULL;
+                metadata* second = (metadata*)myblock + sizeof(metadata*) + size; //this should be the second meta that manages the rest of data
+                meta->next = second;
+                second->isUsed = 0;
+                second->isManaging = blockSize - 2*sizeof(metadata*) - size;
+                second->prev = meta;
+                second-> next = NULL;
+                return myblock + sizeof(metadata);
+
         }else{
             //iterate till you find metadata that fits for your use
+                while(meta->next != NULL){
+                        //traverse until you find an unused chunk or the end of meta chain
+                        if(meta->isUsed == 0 && meta->isManaging >= size + sizeof(meta)){
+                            //found a spot
+                        }
+                        meta = meta->next;
+                        
+                }
+                //At last meta. check size and if not return null
         }
 
 
@@ -31,3 +51,5 @@ void* mymalloc(size_t size, char* file, int line){
 void myfree(void* ptr){
 
 }
+
+
